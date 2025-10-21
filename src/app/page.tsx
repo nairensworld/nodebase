c503e2a64f441ce349ad5411bbcc851228a0fe37
nairens.world <nariens.world@gmail.com>
2025-10-21 21:48:26 +0300

    feat: implement TRPC for API handling and integrate React Query for data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import prisma from "@/lib/db";
+import {  getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const Page = async () => {
-  const users = await prisma.user.findMany();
+
+export default async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
+        
     </div>
   );
-};
-
-export default Page;
+}
