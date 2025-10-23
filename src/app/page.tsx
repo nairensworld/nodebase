1539ae76676a770906ad81f77a4c035afcdef172
nairens.world <nariens.world@gmail.com>
2025-10-23 18:11:18 +0300

    background processes using Inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
-import { auth } from "@/lib/auth";
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-import { headers } from "next/headers";
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { LogoutButton } from "./features/logout-button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job Queued");
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       <div>protected page</div>
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
