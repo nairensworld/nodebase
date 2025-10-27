6b6dd8891d23ab427873dec0315a4776ad8a9b23
nairens.world <nariens.world@gmail.com>
2025-10-27 16:14:20 +0200

    workflows creation

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,26 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import {
+  WorkflowsContainer,
+  WorkflowsList,
+} from "@/app/features/workflows/conmponents/workflows";
 
 const Page = async () => {
   await requireAuth();
 
-  return <div>Workflows</div>;
+  return (
+    <WorkflowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<div>Error</div>}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsContainer>
+  );
 };
 
 export default Page;
